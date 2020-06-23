35ef57c221eb9de367652bda67dc1bf379319209
AxelMusteruus <maximusteruus@gmail.com>
2020-06-24 00:53:22 +0200

    add working correct code

--- pushBot.rb
+++ pushBot.rb
@@ -3,7 +3,7 @@
 class PushBot
   def pushToGitHub(commit_titel)
     system "echo 'something is looking pale' >> 'test_the_difference?' "
-    # a = system 'git diff'
+    a = system 'git diff'
     system "git add ."
     system "git commit -m '#{commit_titel}'"
     branch = `git rev-parse --abbrev-ref HEAD`
