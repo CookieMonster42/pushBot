c59c9d2ed949c634502b40e421facb33cbc0469f
AxelMusteruus <maximusteruus@gmail.com>
2020-06-24 00:51:32 +0200

    commit_titel_101

--- pushBot.rb
+++ pushBot.rb
@@ -1,16 +1,18 @@
 
 
 class PushBot
-  def pushToGitHub(branch, commit_titel)
+  def pushToGitHub(commit_titel)
     system "echo 'something is looking pale' >> 'test_the_difference?' "
-    a = system 'git diff'
+    # a = system 'git diff'
     system "git add ."
     system "git commit -m '#{commit_titel}'"
-    system "git push 'origin #{branch}'"
+    branch = `git rev-parse --abbrev-ref HEAD`
+    system "git push '#{branch}'"
+
   end
 end
 
 
 p = PushBot.new
 
-p.pushToGitHub('master', 'commit_titel_101')
+p.pushToGitHub('commit_titel_101')
