4cd268a6ac96004d158be48a96c40babe680a5ad
AxelMusteruus <maximusteruus@gmail.com>
2020-06-24 11:20:11 +0200

    changed the file

---
+++ pushBot.rb
@@ -0,0 +1,18 @@
+
+
+class PushBot
+  def pushToGitHub(commit_titel)
+    system "echo 'something is looking pale' >> 'test_the_difference?' "
+    a = system 'git diff'
+    system "git add ."
+    system "git commit -m '#{commit_titel}'"
+    branch = `git rev-parse --abbrev-ref HEAD`
+    system "git push 'origin #{branch.strip}'"
+
+  end
+end
+
+
+p = PushBot.new
+
+p.pushToGitHub('commit_titel_101')
