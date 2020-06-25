5491db43ac604e317f373aef5aa63f4fc44e696f
AxelMusteruus <maximusteruus@gmail.com>
2020-06-25 12:20:46 +0200

    changed some code

--- pushBot.rb
+++ pushBot.rb
@@ -2,13 +2,13 @@
 
 class PushBot
   def pushToGitHub(commit_titel)
-    system "echo 'something is looking pale' >> 'test_the_difference?' "
-    a = system 'git diff'
-    system "git add ."
-    system "git commit -m '#{commit_titel}'"
+    # system "echo 'something is looking pale' >> 'test_the_difference?' "
+    # a = system 'git diff'
+    # system "git add ."
+    # system "git commit -m '#{commit_titel}'"
     branch = `git rev-parse --abbrev-ref HEAD`
-    system "git push 'origin #{branch.strip}'"
-
+    puts branch
+    # system "git push 'origin #{branch.strip}'"
   end
 end
 
